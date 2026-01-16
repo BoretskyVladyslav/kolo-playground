14fb545c3f9c17558f15270d27ada8ead2f3e1ca
BoretskyVladyslav <140593999+BoretskyVladyslav@users.noreply.github.com>
2026-01-16 19:35:55 +0200

    fix: remove duplicate config and force mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Вимикаємо перевірку типів під час збірки на сервері
   typescript: {
     ignoreBuildErrors: true,
   },
-  // Вимикаємо перевірку лінтера
   eslint: {
     ignoreDuringBuilds: true,
   },
